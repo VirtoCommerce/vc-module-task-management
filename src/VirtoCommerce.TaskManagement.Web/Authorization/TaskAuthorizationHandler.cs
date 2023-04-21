277e2cafda920fe926bc0dc85fa810a33cf90ceb
Evgeny Okhrimenko <eo@virtoway.com>
2023-04-21 11:56:47 +0200

    fix code smell

--- src/VirtoCommerce.TaskManagement.Web/Authorization/TaskAuthorizationHandler.cs
+++ src/VirtoCommerce.TaskManagement.Web/Authorization/TaskAuthorizationHandler.cs
@@ -35,42 +35,20 @@ namespace VirtoCommerce.TaskManagement.Web.Authorization
             await base.HandleRequirementAsync(context, requirement);
 
             var memberId = context.User.FindFirstValue(MemberIdClaimType);
+            var permission = context.User.FindPermission(requirement.Permission, _jsonOptions.SerializerSettings);
 
-            if (!context.HasSucceeded)
+            if (context.HasSucceeded)
             {
-                var permission = context.User.FindPermission(requirement.Permission, _jsonOptions.SerializerSettings);
-                if (permission != null)
+                if (!context.User.HasGlobalPermission(requirement.Permission))
                 {
-                    var taskToMeScope = permission.AssignedScopes.OfType<TaskToMeScope>().FirstOrDefault();
-                    var taskToMyOrganizationScope = permission.AssignedScopes.OfType<TaskToMyOrganizationScope>().FirstOrDefault();
-                    if (taskToMyOrganizationScope != null)
-                    {
-                        taskToMeScope = null;
-                    }
-
                     switch (context.Resource)
                     {
-                        case WorkTask workTask when taskToMeScope != null && workTask.ResponsibleId == memberId:
+                        case WorkTask workTask:
                             {
                                 var member = await _memberService.GetByIdAsync(workTask.ResponsibleId);
                                 var organizationId = member.GetMemberOrganizationId();
                                 SetResponsible(workTask, member, organizationId);
                                 context.Succeed(requirement);
-                                break;
-                            }
-                        case WorkTask workTask when taskToMyOrganizationScope != null && !string.IsNullOrEmpty(workTask.ResponsibleId):
-                            {
-                                var currentMember = await _memberService.GetByIdAsync(memberId);
-                                var currentMemberOrganizationId = currentMember.GetMemberOrganizationId();
-                                var responsibleMember = await _memberService.GetByIdAsync(workTask.ResponsibleId);
-                                var organizationId = responsibleMember.GetMemberOrganizationId();
-
-                                if (currentMemberOrganizationId == organizationId)
-                                {
-                                    SetResponsible(workTask, responsibleMember, organizationId);
-                                    context.Succeed(requirement);
-                                }
-
                                 break;
                             }
                         case WorkTaskSearchCriteria criteria when criteria.OnlyAssignedToMe == true:
@@ -79,30 +57,32 @@ namespace VirtoCommerce.TaskManagement.Web.Authorization
                                 context.Succeed(requirement);
                                 break;
                             }
-                        case WorkTaskSearchCriteria criteria:
-                            {
-                                var member = await _memberService.GetByIdAsync(memberId);
-                                var organizationId = member.GetMemberOrganizationId();
-
-                                if (!string.IsNullOrEmpty(organizationId) && taskToMyOrganizationScope != null)
-                                {
-                                    criteria.OrganizationIds = new[] { organizationId };
-                                }
-                                else if ((taskToMeScope != null || taskToMyOrganizationScope != null) && !string.IsNullOrEmpty(memberId))
-                                {
-                                    criteria.ResponsibleIds = new[] { memberId };
-                                }
-                                context.Succeed(requirement);
-                                break;
-                            }
                     }
                 }
             }
-            else if (!context.User.HasGlobalPermission(requirement.Permission))
+            else if (permission != null)
             {
+                var taskToMeScope = permission.AssignedScopes.OfType<TaskToMeScope>().FirstOrDefault();
+                var taskToMyOrganizationScope = permission.AssignedScopes.OfType<TaskToMyOrganizationScope>().FirstOrDefault();
+
                 switch (context.Resource)
                 {
-                    case WorkTask workTask:
+                    case WorkTask workTask when taskToMyOrganizationScope != null:
+                        {
+                            var currentMember = await _memberService.GetByIdAsync(memberId);
+                            var currentMemberOrganizationId = currentMember.GetMemberOrganizationId();
+                            var responsibleMember = await _memberService.GetByIdAsync(workTask.ResponsibleId);
+                            var organizationId = responsibleMember.GetMemberOrganizationId();
+
+                            if (currentMemberOrganizationId == organizationId)
+                            {
+                                SetResponsible(workTask, responsibleMember, organizationId);
+                                context.Succeed(requirement);
+                            }
+
+                            break;
+                        }
+                    case WorkTask workTask when taskToMeScope != null && workTask.ResponsibleId == memberId:
                         {
                             var member = await _memberService.GetByIdAsync(workTask.ResponsibleId);
                             var organizationId = member.GetMemberOrganizationId();
@@ -110,13 +90,29 @@ namespace VirtoCommerce.TaskManagement.Web.Authorization
                             context.Succeed(requirement);
                             break;
                         }
-                    case WorkTaskSearchCriteria criteria when criteria.OnlyAssignedToMe == true:
+                    case WorkTaskSearchCriteria criteria:
                         {
-                            criteria.ResponsibleIds = new[] { memberId };
+                            var isSearchOrganizationTasks = taskToMyOrganizationScope != null;
+                            await SetCriteria(criteria, isSearchOrganizationTasks, memberId);
                             context.Succeed(requirement);
                             break;
                         }
                 }
+            }
+        }
+
+        private async Task SetCriteria(WorkTaskSearchCriteria criteria, bool isSearchOrganizationTasks, string memberId)
+        {
+            var member = await _memberService.GetByIdAsync(memberId);
+            var organizationId = member.GetMemberOrganizationId();
+
+            if (!string.IsNullOrEmpty(organizationId) && isSearchOrganizationTasks && !(criteria.OnlyAssignedToMe ?? false))
+            {
+                criteria.OrganizationIds = new[] { organizationId };
+            }
+            else
+            {
+                criteria.ResponsibleIds = new[] { memberId };
             }
         }
 
@@ -125,5 +121,7 @@ namespace VirtoCommerce.TaskManagement.Web.Authorization
             workTask.ResponsibleName = member?.Name;
             workTask.OrganizationId = organizationId;
         }
+
+
     }
 }

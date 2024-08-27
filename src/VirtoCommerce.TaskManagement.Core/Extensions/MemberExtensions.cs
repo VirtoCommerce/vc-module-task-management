a73833056334a816af0364d56bbabf3be9ec3f15
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2024-08-27 15:06:27 +0200

    feat: Use current/default organization ID

--- src/VirtoCommerce.TaskManagement.Core/Extensions/MemberExtensions.cs
+++ src/VirtoCommerce.TaskManagement.Core/Extensions/MemberExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using VirtoCommerce.CustomerModule.Core.Model;
 
@@ -5,12 +6,23 @@ namespace VirtoCommerce.TaskManagement.Core.Extensions
 {
     public static class MemberExtensions
     {
+        [Obsolete("Use GetMemberDefaultOrganizationId")]
         public static string GetMemberOrganizationId(this Member member)
         {
             return member?.MemberType switch
             {
                 nameof(Contact) => (member as Contact)?.Organizations?.FirstOrDefault(),
                 nameof(Employee) => (member as Employee)?.Organizations?.FirstOrDefault(),
+                _ => null
+            };
+        }
+
+        public static string GetMemberDefaultOrganizationId(this Member member)
+        {
+            return member?.MemberType switch
+            {
+                nameof(Contact) => (member as Contact)?.DefaultOrganizationId,
+                nameof(Employee) => (member as Employee)?.DefaultOrganizationId,
                 _ => null
             };
         }
